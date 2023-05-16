cd8533f64acc78b18a5c968988a763b883e61e75
Johannes Krabbe <Johannes@Krabbe.net>
2023-05-16 19:11:51 +0200

    error handeling

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -27,11 +27,48 @@ authController.post("/signup", async (req, res) => {
     bio: string;
   };
 
+  // check if all neccecarry params got send
+  if (!username || !email || !password) {
+    res.status(400).json({
+      error: true,
+      message: "Please provide a username, email and password",
+    });
+    return;
+  }
+
+  // check if username is valid
+  const pattern = /^[a-z0-9-_]+$/;
+  if (username.length > 32 || !pattern.test(username)) {
+    res.status(400).json({
+      error: true,
+      message: "Username is not valid",
+    });
+    return;
+  }
+
+  // check if username or email is already in use
+  const usernameUser = await prisma.user.findUnique({ where: { username } });
+  const emailUser = await prisma.user.findUnique({ where: { email } });
+  if (usernameUser || emailUser) {
+    res.status(400).json({
+      error: true,
+      message: "Username or Email is already in use",
+    });
+    return;
+  }
+
+  // create user
   const hashedPass = await argon2.hash(password);
   const newUser = await prisma.user.create({
-    data: { username, email, password: hashedPass, bio },
+    data: {
+      username,
+      email,
+      password: hashedPass,
+      bio: bio ? bio : "Hello there, I am using Dunkeytype!",
+    },
   });
 
+  // generate JWT
   const jwt = createJWT(newUser.id);
 
   res.status(201).json({ token: jwt });
