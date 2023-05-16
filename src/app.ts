45627ac4eb343edd94965b21970ac81ff4b64085
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2023-05-16 20:39:49 +0200

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 const port = parseInt(process.env.PORT || "8080");
 
 app.use(express.json());
-
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 app.use(router);
 
 if (process.env.NODE_ENV !== "develop") {
