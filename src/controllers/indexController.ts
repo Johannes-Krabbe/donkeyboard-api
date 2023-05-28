726795c12e4792a741506014fbca3d4ef357b7f8
Johannes Krabbe <Johannes@Krabbe.net>
2023-05-28 15:45:39 +0200

    test deploy

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -6,13 +6,17 @@ export const indexController = Router();
 indexController.get(
   "/",
   asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).send("This is the Donkeyboard api");
+    res
+      .status(200)
+      .send(`This is the <span style="color:red">donkeyboard</span> api!`);
   })
 );
 
 indexController.post(
   "/",
   asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).send("This is the Donkeyboard api");
+    res
+      .status(200)
+      .send("This is the donkeyboard api! POST request was successful");
   })
 );
