5fc63d7122ad2f36290c2b45469dbd151c1c7950
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2023-05-28 20:45:04 +0200

    temp regex fix

--- src/controllers/auth/authValidation.ts
+++ src/controllers/auth/authValidation.ts
@@ -4,9 +4,13 @@ const username = z
   .string()
   .refine((value) => /^[a-z0-9-_]{3,32}$/.test(value), "ERROR");
 
-const email = z
-  .string()
-  .regex(new RegExp(/^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$/), "ERROR");
+const email = z.string();
+/*
+  .refine(
+    (value) => /^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$/.test(value),
+    "ERROR"
+  );
+*/
 
 export const registerBodySchema = z.object({
   username,
